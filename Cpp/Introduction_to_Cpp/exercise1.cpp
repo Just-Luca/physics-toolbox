e2f2da17a33f889ea71a1aee7e3437c194111bb6
Luca Giusto <168096340+Just-Luca@users.noreply.github.com>
2026-04-23 01:47:05 +0200

    Change 'rapporto' to 'ratio' in exercise1.cpp

--- Cpp/Introduction_to_Cpp/exercise1.cpp
+++ Cpp/Introduction_to_Cpp/exercise1.cpp
@@ -2,12 +2,11 @@
 
 c++ -o exercise1 exercise1.cpp
  
-Exercise: write a code so that, given two natural numbers from terminal, it prints the fraction of the two
+Exercise: write a code so that, given two natural numbers from terminal, it prints the ratio of the two
 
 */
 
 #include <iostream>
-come si dice rapporto in inglese
 
 int main () {
 
@@ -19,10 +18,10 @@ int main () {
   std::cout << "Second number: " << std::endl;
   std::cin >> number2;
 
-  float rapporto = (float)number1 / number2;
+  float ratio = (float)number1 / number2;
 
-  std::cout << "Il rapporto tra i due numeri è: "
-      << rapporto << std::endl;
+  std::cout << "The ratio is: "
+      << ratio << std::endl;
 
   return 0;
 }
